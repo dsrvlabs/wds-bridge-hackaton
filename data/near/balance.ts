6ac72daf0dd29f1b3196c05d1dcb4d071eec8111
kwklly <yeonlee@i.ewha.ac.kr>
2021-09-13 13:29:39 +0900

    #10 modify balance handler

--- data/near/balance.ts
+++ data/near/balance.ts
@@ -1,56 +1,60 @@
-import { Balance, BalanceError } from '../types';
+import { BalanceError, ApiResponse } from '@/data/types';
 
-export class Near {
-  static async getBalance(address: string): Promise<Balance> {
-    const URL = 'https://rpc.mainnet.near.org';
-    const helperURL = 'https://helper.mainnet.near.org';
-    let balanceError: BalanceError;
-    if (address.includes(':')) {
-      const helper = `${helperURL}/publicKey/${address}/accounts`;
-      const accountIds = await fetch(helper).then((res) => {
-        return res.json();
-      });
-      address = accountIds[Object.keys(accountIds).length - 1];
-    }
-    if (address) {
-      try {
-        const response = await fetch(URL, {
-          method: 'POST',
-          headers: {
-            Accept: '*/*',
-            'Content-Type': 'application/json',
+export const getBalance = async (address: string): Promise<ApiResponse> => {
+  const URL = 'https://rpc.mainnet.near.org';
+  const helperURL = 'https://helper.mainnet.near.org';
+  let balanceError: BalanceError;
+  if (address.includes(':')) {
+    const helper = `${helperURL}/publicKey/${address}/accounts`;
+    const accountIds = await fetch(helper).then((res) => {
+      return res.json();
+    });
+    address = accountIds[Object.keys(accountIds).length - 1];
+  }
+  if (address) {
+    try {
+      const response = await fetch(URL, {
+        method: 'POST',
+        headers: {
+          Accept: '*/*',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          jsonrpc: '2.0',
+          id: 'dontcare',
+          method: 'query',
+          params: {
+            request_type: 'view_account',
+            finality: 'final',
+            account_id: address,
           },
-          body: JSON.stringify({
-            jsonrpc: '2.0',
-            id: 'dontcare',
-            method: 'query',
-            params: {
-              request_type: 'view_account',
-              finality: 'final',
-              account_id: address,
-            },
-          }),
-        });
-        const block = await response.json();
-        const balance: Balance = {
+        }),
+      });
+      const block = await response.json();
+      const balance: ApiResponse = {
+        api: 'balance',
+        data: {
           decimal: 24,
           total: block.result.amount,
           locked: block.result.locked,
-          error: BalanceError.NO_ERROR,
-        };
-        return balance;
-      } catch (error) {
-        balanceError = BalanceError.ADDRESS_ERROR;
-      }
-    } else {
-      balanceError = BalanceError.NO_ADDRESS;
+        },
+        error: BalanceError.NO_ERROR,
+      };
+      return balance;
+    } catch (error) {
+      balanceError = BalanceError.ADDRESS_ERROR;
     }
-    const balance: Balance = {
+  } else {
+    balanceError = BalanceError.NO_ADDRESS;
+  }
+  const balance: ApiResponse = {
+    api: 'balance',
+    data: {
       decimal: 9,
       total: '',
       locked: '',
-      error: balanceError,
-    };
-    return balance;
-  }
-}
+    },
+    error: balanceError,
+  };
+  return balance;
+};
