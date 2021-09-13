50ab690565fa898d9185d6ae2810581cf8965971
kwklly <yeonlee@i.ewha.ac.kr>
2021-09-13 16:29:56 +0900

    #10 modify stake handler

--- pages/api/stake/[chain]/[address].tsx
+++ pages/api/stake/[chain]/[address].tsx
@@ -1,32 +1,24 @@
 import type { NextApiResponse } from 'next';
-import { Solana } from '../../../../data/solana/stake';
-import { Near } from '../../../../data/near/stake';
-import { Stake, BalanceError, StakeStatus } from '../../../../data/types';
+import { getStake as Solana } from '@/data/solana/stake';
+import { getStake as Near } from '@/data/near/stake';
+import { BalanceError, ApiResponse } from '@/data/types';
+
 export default async function handler(req: any, res: NextApiResponse): Promise<void> {
   const address = req.query.address;
   const chain = req.query.chain;
-  let stakes: Stake[];
+  let response: ApiResponse;
 
   switch (chain) {
     case 'solana':
-      stakes = await Solana.getStake(address);
-      res.status(200).json({ stakes });
+      response = await Solana(address);
+      res.status(200).json(response);
       break;
     case 'near':
-      stakes = await Near.getStake(address);
-      res.status(200).json({ stakes });
+      response = await Near(address);
+      res.status(200).json(response);
       break;
     default:
-      stakes = [
-        {
-          validator: { address: '' },
-          value: '',
-          status: StakeStatus.NOT_DELEGATED,
-          error: BalanceError.NOT_SUPPORTED_CHAIN,
-          stakeAccount: '',
-        },
-      ];
-      res.status(200).json({ stakes });
+      res.status(200).json({ error: BalanceError.NOT_SUPPORTED_CHAIN });
       break;
   }
 }
