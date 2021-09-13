c8a03df3bdfea2d13bc0171dfd4bd5c91403f3f5
kwklly <yeonlee@i.ewha.ac.kr>
2021-09-13 12:03:01 +0900

    #12 modify price handler

--- pages/api/price/[chain].ts
+++ pages/api/price/[chain].ts
@@ -1,11 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import type { NextApiResponse } from 'next';
-import { Price, PriceError } from '../../../data/types';
+import { PriceError, ApiResponse } from '@/data/types';
 
 export default async function handler(req: any, res: NextApiResponse): Promise<void> {
   const chain = req.query.chain;
   const URL = 'https://api.coingecko.com/api/v3';
-  let price: Price;
+  let price: ApiResponse;
   let data: any;
 
   switch (chain) {
@@ -32,16 +32,19 @@ export default async function handler(req: any, res: NextApiResponse): Promise<v
         )
       ).json();
       price = {
-        now: data.market_data.current_price.usd || null,
-        high24h: data.market_data.high_24h.usd || null,
-        low24h: data.market_data.low_24h.usd || null,
-        change7d: data.market_data.price_change_percentage_7d_in_currency.usd || null,
-        change14d: data.market_data.price_change_percentage_14d_in_currency.usd || null,
-        change30d: data.market_data.price_change_percentage_30d_in_currency.usd || null,
-        change1y: data.market_data.price_change_percentage_1y_in_currency.usd || null,
+        api: 'price',
+        data: {
+          now: data.market_data.current_price.usd || null,
+          high24h: data.market_data.high_24h.usd || null,
+          low24h: data.market_data.low_24h.usd || null,
+          change7d: data.market_data.price_change_percentage_7d_in_currency.usd || null,
+          change14d: data.market_data.price_change_percentage_14d_in_currency.usd || null,
+          change30d: data.market_data.price_change_percentage_30d_in_currency.usd || null,
+          change1y: data.market_data.price_change_percentage_1y_in_currency.usd || null,
+        },
         error: PriceError.NO_ERROR,
       };
-      res.status(200).json({ price });
+      res.status(200).json(price);
       break;
     default:
       res.status(200).json({ error: PriceError.NOT_SUPPORTED_CHAIN });
