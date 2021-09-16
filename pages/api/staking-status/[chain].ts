cbd113a8a1b37169fc86de17dfce26d359479b84
kwklly <yeonlee@i.ewha.ac.kr>
2021-09-16 10:14:07 +0900

    modify api-status

--- pages/api/staking-status/[chain].ts
+++ pages/api/staking-status/[chain].ts
@@ -1,47 +1,26 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import type { NextApiResponse } from 'next';
 import { StakingStatusError, ApiResponse } from '@/data/types';
+import { ChainConverter } from '@/data/chaininfo/chain-list-infos';
 
 export default async function handler(req: any, res: NextApiResponse): Promise<void> {
   const chain = req.query.chain;
-  let apiResponse: ApiResponse;
-  switch (chain) {
-    case 'agoric':
-    case 'celo':
-    case 'cosmos':
-    case 'flow':
-    case 'kusama':
-    case 'lido':
-    case 'mina':
-    case 'near':
-    case 'persistence':
-    case 'polkadot':
-    case 'polygon':
-    case 'solana':
-    case 'thegraph':
-    case 'thorchain':
-    case 'tokamak':
-    case 'terra':
-      /*
-      TO DO: DB QUERY
-      */
-      apiResponse = {
-        api: '',
-        data: {
-          staking: 0,
-          delegators: 0,
-          reward: 0,
-          comission: 10,
-        },
-        error: StakingStatusError.NO_ERROR,
-      };
-      break;
-    default:
-      apiResponse = {
-        api: '',
-        error: StakingStatusError.NOT_SUPPORTED_CHAIN,
-      };
-      break;
+  let apiResponse: ApiResponse = {
+    api: '',
+    error: StakingStatusError.NOT_SUPPORTED_CHAIN,
+  };
+  // TODO: DB QUERY
+  if (ChainConverter[chain]) {
+    apiResponse = {
+      api: '',
+      data: {
+        staking: 0,
+        delegators: 0,
+        reward: 0,
+        comission: 10,
+      },
+      error: StakingStatusError.NO_ERROR,
+    };
   }
   res.status(200).json({ ...apiResponse, api: `v1/staking-status/${chain}` });
 }
